074cbfa89c9101f89c2ee60baa9cf514e783a4d9
zRalphy <rafalmec@gmail.com>
2021-10-08 12:16:25 +0200

    Added LOGGER functionality to project.

--- src/main/java/com/pgs/booking/controller/PassengerController.java
+++ src/main/java/com/pgs/booking/controller/PassengerController.java
@@ -5,6 +5,8 @@ import com.pgs.booking.model.dto.CreateUpdatePassengerDto;
 import com.pgs.booking.model.dto.PassengerDto;
 import com.pgs.booking.service.PassengerService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -13,24 +15,29 @@ import java.util.List;
 public class PassengerController {
 
     private final PassengerService passengerService;
+    private static final Logger LOGGER = LoggerFactory.getLogger(PassengerController.class);
 
     @GetMapping("/api/passengers")
     public List<PassengerDto> getPassengers() {
+        LOGGER.info("Inside method: getPassengers.");
         return passengerService.getPassengers();
     }
 
     @GetMapping("/api/passengers/{id}")
     public PassengerDto getSinglePassenger(@PathVariable long id)  {
+        LOGGER.info("Inside method: getSinglePassenger.");
             return passengerService.getSinglePassenger(id);
     }
 
     @PostMapping("/api/passengers")
     public Passenger addPassenger(@RequestBody CreateUpdatePassengerDto createUpdatePassengerDto) {
+        LOGGER.info("Inside method: addPassenger.");
         return passengerService.addNewPassenger(createUpdatePassengerDto);
     }
 
     @PutMapping("/api/passengers/{id}")
     public Passenger editPassenger(@RequestBody CreateUpdatePassengerDto createUpdatePassengerDto, @PathVariable long id) {
+        LOGGER.info("Inside method: editPassenger.");
         return passengerService.editPassenger(id, createUpdatePassengerDto);
     }
 }
