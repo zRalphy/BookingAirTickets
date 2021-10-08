7f7ee5d089b5cc5ec678e72af448fa46c1ec14de
zRalphy <rafalmec@gmail.com>
2021-10-08 13:35:35 +0200

    Added spring-boot-starter-validation to pom file and use validation in passenger class.

--- src/main/java/com/pgs/booking/model/Passenger.java
+++ src/main/java/com/pgs/booking/model/Passenger.java
@@ -1,10 +1,13 @@
 package com.pgs.booking.model;
 
-import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
@@ -17,14 +20,27 @@ public class Passenger {
     @Column(name = "id")
     private final Long id = null;
     @Column(name = "firstName")
+    @NotEmpty
+    @NotBlank(message = "Please enter your firstName.")
+    @Size(min = 2, message = "Passenger firstName should have at least 2 characters.")
     private String firstName;
     @Column(name = "lastName")
+    @NotEmpty
+    @NotBlank(message = "Please enter your lastName.")
+    @Size(min = 2, message = "Passenger lastName should have at least 2 characters.")
     private String lastName;
     @Column(name = "email")
+    @NotEmpty
+    @NotBlank(message = "Please enter your email.")
+    @Email
     private String email;
     @Column(name = "country")
+    @NotEmpty
+    @NotBlank(message = "Please enter your country.")
     private String country;
     @Column(name = "telephone")
+    @NotEmpty
+    @NotBlank(message = "Please enter your phoneNumber.")
     private String telephone;
 }
 
--- src/main/java/com/pgs/booking/repository/PassengerRepository.java
+++ src/main/java/com/pgs/booking/repository/PassengerRepository.java
@@ -1,12 +1,11 @@
 package com.pgs.booking.repository;
 
 import com.pgs.booking.model.Passenger;
-import com.pgs.booking.model.dto.PassengerDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface PassengerRepository extends JpaRepository<Passenger, Long> {
+public interface PassengerRepository extends JpaRepository<Passenger, Long>{
     Optional<Passenger> findByEmail(String email);
 }
