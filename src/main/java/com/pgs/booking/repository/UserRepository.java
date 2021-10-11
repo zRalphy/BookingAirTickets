48d291e11ddb9b384d7641009b8ba20b6a653eee
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-10-11 16:54:33 +0200

    Implemented UserRepository, which extends JpaRepository implemented findByUsername abstract method

---
+++ src/main/java/com/pgs/booking/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.pgs.booking.repository;
+
+import com.pgs.booking.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
