c0ff4ec262fc1681a29a19161481baa273d8a2d9
zRalphy <rafalmec@gmail.com>
2021-10-11 16:19:28 +0200

    Next validated after rebase.

--- src/main/java/com/pgs/booking/exception/ApiExceptionHandler.java
+++ src/main/java/com/pgs/booking/exception/ApiExceptionHandler.java
@@ -6,11 +6,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import java.util.Date;
 
 @Slf4j
 @ControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<ErrorResponse> handleAllExceptions(Exception ex, WebRequest request) {
