7902a11af498bef34830893b26a5820efc4e7df6
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-10-13 16:17:00 +0200

    class implements another interface AuthenticationUserDetailsService<PreAuthenticatedAuthenticationToken> implemented method from AuthenticationUserDetailsService

--- src/main/java/com/pgs/booking/service/UserService.java
+++ src/main/java/com/pgs/booking/service/UserService.java
@@ -3,15 +3,18 @@ package com.pgs.booking.service;
 import com.pgs.booking.model.User;
 import com.pgs.booking.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.AuthenticationUserDetailsService;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class UserService implements UserDetailsService {
+public class UserService implements UserDetailsService, AuthenticationUserDetailsService<PreAuthenticatedAuthenticationToken> {
 
     private final UserRepository userRepository;
 
@@ -19,5 +22,10 @@ public class UserService implements UserDetailsService {
     public User loadUserByUsername(String username) throws UsernameNotFoundException {
         return Optional.ofNullable(userRepository.findByUsername(username))
                 .orElseThrow(() -> new UsernameNotFoundException(username));
+    }
+
+    @Override
+    public UserDetails loadUserDetails(PreAuthenticatedAuthenticationToken token) throws UsernameNotFoundException {
+        return loadUserByUsername(token.getName());
     }
 }
