229b6411417f31808061151f3a8ceb97cb524d91
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-10-13 11:00:21 +0200

    implemented override methods: configure with parameter AuthorizationServerSecurityConfigurer - token key checks authenticated for all requests configure with parameter ClientDetailsServiceConfigurer  - in memory client with client-id,  password and specified grant types implemented beans: tokenStore defaultAccessTokenConverter

---
+++ src/main/java/com/pgs/booking/configuration/OauthServerConfiguration.java
@@ -0,0 +1,52 @@
+package com.pgs.booking.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+@RequiredArgsConstructor
+public class OauthServerConfiguration extends AuthorizationServerConfigurerAdapter {
+
+
+    private final AuthenticationManager authenticationManager;
+    private final TokenStore tokenStore;
+    private final UserDetailsService userDetailsService;
+
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+        oauthServer.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("ClientId").secret("secret")
+                .authorizedGrantTypes("password", "authorization_code", "refresh_token")
+                .autoApprove(true);
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(defaultAccessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter defaultAccessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey("123");
+        return converter;
+    }
+
+}
