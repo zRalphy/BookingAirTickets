611c1cb07f9decc43ba6b8effd4604ba153f6f8b
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-10-13 16:12:12 +0200

    modified methods: configure with ClientDetailsServiceConfigurer - added scopes, and encoder for the secret configure with AuthorizationServerEndpointsConfigurer - changed name of parameter for clarity

--- src/main/java/com/pgs/booking/configuration/OauthServerConfiguration.java
+++ src/main/java/com/pgs/booking/configuration/OauthServerConfiguration.java
@@ -4,15 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.context.annotation.Primary;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
@@ -25,14 +24,16 @@ public class OauthServerConfiguration extends AuthorizationServerConfigurerAdapt
     private AuthenticationManager authenticationManager;
     private TokenStore tokenStore;
     private UserDetailsService userDetailsService;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
     public OauthServerConfiguration(UserDetailsService userDetailsService,
                                     AuthenticationManager authenticationManager,
-                                    @Lazy TokenStore tokenStore) {
+                                    @Lazy TokenStore tokenStore, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userDetailsService = userDetailsService;
         this.authenticationManager = authenticationManager;
         this.tokenStore = tokenStore;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     @Override
@@ -43,27 +44,19 @@ public class OauthServerConfiguration extends AuthorizationServerConfigurerAdapt
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory()
-                .withClient("ClientId").secret("secret")
+                .withClient("ClientId").secret("secret").scopes("read", "write")
                 .authorizedGrantTypes("password", "authorization_code", "refresh_token")
+                .secret(bCryptPasswordEncoder.encode("password"))
                 .autoApprove(true);
     }
 
     @Override
-    public void configure(final AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints
+    public void configure(AuthorizationServerEndpointsConfigurer endpointsConfigurer) throws Exception {
+        endpointsConfigurer
                 .tokenStore(tokenStore())
-                .tokenServices(tokenServices())
                 .authenticationManager(authenticationManager)
                 .accessTokenConverter(accessTokenConverter())
                 .userDetailsService(userDetailsService);
-    }
-
-    @Bean
-    @Primary
-    public DefaultTokenServices tokenServices() {
-        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
-        defaultTokenServices.setTokenStore(tokenStore);
-        return defaultTokenServices;
     }
 
     @Bean
