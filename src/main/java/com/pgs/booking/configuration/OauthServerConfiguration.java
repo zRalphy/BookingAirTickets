38c7c2a381729d57fd2c401f2caee86a7a128349
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-10-13 13:53:24 +0200

    Changed injection of field from @Required args constructor to custom constructor override configure method implemented bean tokenService changed name of bean to accessTokenConverter

--- src/main/java/com/pgs/booking/configuration/OauthServerConfiguration.java
+++ src/main/java/com/pgs/booking/configuration/OauthServerConfiguration.java
@@ -1,28 +1,39 @@
 package com.pgs.booking.configuration;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
-@RequiredArgsConstructor
 public class OauthServerConfiguration extends AuthorizationServerConfigurerAdapter {
 
 
-    private final AuthenticationManager authenticationManager;
-    private final TokenStore tokenStore;
-    private final UserDetailsService userDetailsService;
+    private AuthenticationManager authenticationManager;
+    private TokenStore tokenStore;
+    private UserDetailsService userDetailsService;
 
+    @Autowired
+    public OauthServerConfiguration(UserDetailsService userDetailsService,
+                                    AuthenticationManager authenticationManager,
+                                    @Lazy TokenStore tokenStore) {
+        this.userDetailsService = userDetailsService;
+        this.authenticationManager = authenticationManager;
+        this.tokenStore = tokenStore;
+    }
 
     @Override
     public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
@@ -37,13 +48,31 @@ public class OauthServerConfiguration extends AuthorizationServerConfigurerAdapt
                 .autoApprove(true);
     }
 
-    @Bean
-    public TokenStore tokenStore() {
-        return new JwtTokenStore(defaultAccessTokenConverter());
+    @Override
+    public void configure(final AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints
+                .tokenStore(tokenStore())
+                .tokenServices(tokenServices())
+                .authenticationManager(authenticationManager)
+                .accessTokenConverter(accessTokenConverter())
+                .userDetailsService(userDetailsService);
     }
 
     @Bean
-    public JwtAccessTokenConverter defaultAccessTokenConverter() {
+    @Primary
+    public DefaultTokenServices tokenServices() {
+        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+        defaultTokenServices.setTokenStore(tokenStore);
+        return defaultTokenServices;
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
         JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
         converter.setSigningKey("123");
         return converter;
