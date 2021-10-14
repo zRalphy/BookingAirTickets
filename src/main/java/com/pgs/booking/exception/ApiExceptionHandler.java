4ce942be655a1af02533ab831f73c1987fb07f49
zRalphy <rafalmec@gmail.com>
2021-10-14 15:48:03 +0200

    Removed handleValidationExceptions from the PassengerController and moved this functionality to the global handler. Added RequestMapping for PassengerController.

--- src/main/java/com/pgs/booking/exception/ApiExceptionHandler.java
+++ src/main/java/com/pgs/booking/exception/ApiExceptionHandler.java
@@ -1,29 +1,42 @@
 package com.pgs.booking.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import java.util.Date;
+import static java.util.stream.Collectors.*;
 
 @Slf4j
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
-    public final ResponseEntity<ErrorResponse> handleAllExceptions(Exception ex, WebRequest request) {
-        if(ex instanceof ResourceNotFoundException){
-            ErrorResponse error = new ErrorResponse(new Date(), ex.getMessage(), request.getDescription(false));
-            log.error("Resource was not found.", ex);
-            return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-        } else {
-            HttpStatus internalConflict = HttpStatus.INTERNAL_SERVER_ERROR;
-            ErrorResponse error = new ErrorResponse(new Date(), ex.getMessage(), request.getDescription(false));
-            log.error("Oops, internal server error.", ex);
-            return new ResponseEntity<>(error, internalConflict);
-        }
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public final ResponseEntity<ErrorResponse> handleAllExceptions(ResourceNotFoundException ex, WebRequest request) {
+        ErrorResponse error = new ErrorResponse(new Date(), ex.getMessage(), request.getDescription(false));
+        log.error("Resource was not found.", ex);
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatus status,
+                                                                  WebRequest request) {
+        var response = ex.getBindingResult().getAllErrors().stream()
+                .collect(groupingBy(this::getField, mapping(ObjectError::getDefaultMessage, toList())));
+
+        return ResponseEntity.badRequest().body(response);
+    }
+
+    private String getField(ObjectError error) {
+        return ((FieldError) error).getField();
     }
 }
