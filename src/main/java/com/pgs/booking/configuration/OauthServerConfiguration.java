f114c02f091599cfa2a5cb07a5b212e0b3e96ce5
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-10-14 16:01:33 +0200

    modified injection type to interface: PasswordEncoder TokenStore removed redundant method call resourceIds  in configure method

--- src/main/java/com/pgs/booking/configuration/OauthServerConfiguration.java
+++ src/main/java/com/pgs/booking/configuration/OauthServerConfiguration.java
@@ -4,12 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 
 @Configuration
 @EnableAuthorizationServer
@@ -18,8 +18,8 @@ public class OauthServerConfiguration extends AuthorizationServerConfigurerAdapt
 
     private final AuthenticationManager authenticationManager;
     private final UserDetailsService userDetailsService;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
-    private final JwtTokenStore tokenStore;
+    private final PasswordEncoder bCryptPasswordEncoder;
+    private final TokenStore tokenStore;
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
@@ -28,15 +28,15 @@ public class OauthServerConfiguration extends AuthorizationServerConfigurerAdapt
                 .secret(bCryptPasswordEncoder.encode("secret"))
                 .accessTokenValiditySeconds(60)
                 .scopes("read", "write")
-                .authorizedGrantTypes("password", "authorization_code", "refresh_token")
-                .resourceIds("resource");
+                .authorizedGrantTypes("password", "authorization_code", "refresh_token");
     }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpointsConfigurer) throws Exception {
         endpointsConfigurer
+                .tokenStore(tokenStore)
                 .authenticationManager(authenticationManager)
-                .userDetailsService(userDetailsService)
-                .tokenStore(tokenStore);
+                .userDetailsService(userDetailsService);
+
     }
 }
