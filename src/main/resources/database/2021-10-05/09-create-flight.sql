7ff4c5a05bee8619ec053a23cc860ea00f55d47f
zRalphy <rafalmec@gmail.com>
2021-10-15 10:51:19 +0200

    Created table flight.

---
+++ src/main/resources/database/2021-10-05/09-create-flight.sql
@@ -0,0 +1,10 @@
+-- liquibase formatted sql
+-- changeset rmecwaldowski:2
+CREATE TABLE flight(
+	id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    type VARCHAR(45) NOT NULL,
+    typeOfSeat VARCHAR(45) NOT NULL,
+    typeOfLuggage VARCHAR(45) NOT NULL,
+    departureDate TIMESTAMP,
+    dateOfArrival TIMESTAMP
+);
