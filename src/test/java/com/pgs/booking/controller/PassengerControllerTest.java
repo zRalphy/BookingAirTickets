58f7c1c2dcfdebe8a0c9db0ba05b537e74e0b944
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-10-19 09:27:23 +0200

    removed methods that are no longer in use

--- src/test/java/com/pgs/booking/controller/PassengerControllerTest.java
+++ src/test/java/com/pgs/booking/controller/PassengerControllerTest.java
@@ -9,32 +9,24 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.json.JacksonJsonParser;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebAppConfiguration
 @SpringBootTest(classes = BookingAirTicketsApplication.class)
 class PassengerControllerTest {
 
@@ -58,20 +50,15 @@ class PassengerControllerTest {
     @BeforeEach
     public void setUP() {
         mockMvc = MockMvcBuilders
-                .webAppContextSetup(wac)
-                .apply(springSecurity())
-                .build();
+                .webAppContextSetup(wac).build();
     }
 
     @SneakyThrows
     @Test
     void testGetPassengers() {
-        String accessToken = obtainAccessToken();
         var passengerDtoList = List.of(PASSENGER_DTO);
         given(passengerService.getPassengers()).willReturn(passengerDtoList);
-        mockMvc.perform(get("/api/passengers")
-                        .header("Authorization", "Bearer " + accessToken)
-                        .accept("application/json;charset=UTF-8"))
+        mockMvc.perform(get("/api/passengers"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(1)))
                 .andExpect(jsonPath("$[0].id", equalTo(PASSENGER_DTO.getId().intValue())))
@@ -86,13 +73,9 @@ class PassengerControllerTest {
     @SneakyThrows
     @Test
     void testGetSinglePassenger() {
-        String accessToken = obtainAccessToken();
         given(passengerService.getSinglePassenger(5L)).willReturn(PASSENGER_DTO);
-        mockMvc.perform(get("/api/passengers/5")
-                        .header("Authorization", "Bearer " + accessToken)
-                        .accept("application/json;charset=UTF-8"))
+        mockMvc.perform(get("/api/passengers/5"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType("application/json;charset=UTF-8"))
                 .andExpect(jsonPath("id", equalTo(PASSENGER_DTO.getId().intValue())))
                 .andExpect(jsonPath("firstName", equalTo(PASSENGER_DTO.getFirstName())))
                 .andExpect(jsonPath("lastName", equalTo(PASSENGER_DTO.getLastName())))
@@ -100,51 +83,5 @@ class PassengerControllerTest {
                 .andExpect(jsonPath("country", equalTo(PASSENGER_DTO.getCountry())))
                 .andExpect(jsonPath("telephone", equalTo(PASSENGER_DTO.getTelephone())));
         verify(passengerService).getSinglePassenger(5L);
-    }
-
-    @SneakyThrows
-    @Test
-    void testGetPassengersUnauthenticated() {
-        var passengerDtoList = List.of(PASSENGER_DTO);
-        given(passengerService.getPassengers()).willReturn(passengerDtoList);
-        mockMvc.perform(get("/api/passengers"))
-                .andExpect(status().isUnauthorized());
-    }
-
-    @SneakyThrows
-    @Test
-    void testGetSinglePassengerUnauthenticated() {
-        given(passengerService.getSinglePassenger(5L)).willReturn(PASSENGER_DTO);
-        mockMvc.perform(get("/api/passengers/5"))
-                .andExpect(status().isUnauthorized());
-    }
-
-    @SneakyThrows
-    private String obtainAccessToken() {
-        ResultActions resultActions = performActionForOauthToken();
-        String resultString = resultActions.andReturn().getResponse().getContentAsString();
-        JacksonJsonParser jsonParser = new JacksonJsonParser();
-        return jsonParser.parseMap(resultString).get("access_token").toString();
-    }
-
-    @SneakyThrows
-    private ResultActions performActionForOauthToken() {
-        return mockMvc
-                .perform(post("/oauth/token")
-                        .content(setParamsForRequest("user", "user"))
-                        .with(httpBasic("ClientId", "secret"))
-                        .accept("application/json;charset=UTF-8"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType("application/json;charset=UTF-8"));
-    }
-
-    @SneakyThrows
-    private String setParamsForRequest(String username, String password) {
-        Map<String, String> params = new HashMap<>();
-        params.put("grant_type", "password");
-        params.put("username", username);
-        params.put("password", password);
-
-        return objectMapper.writeValueAsString(params);
     }
 }
