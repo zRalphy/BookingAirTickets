e228e5ad2560ee17139ea4757673a5caa20e3a60
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-10-19 16:04:19 +0200

    added annotations for Role

--- src/main/java/com/pgs/booking/model/Role.java
+++ src/main/java/com/pgs/booking/model/Role.java
@@ -1,6 +1,8 @@
 package com.pgs.booking.model;
 
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,6 +11,8 @@ import javax.persistence.Table;
 
 @Entity
 @Data
+@Builder
+@NoArgsConstructor
 @Table(name = "role")
 public class Role {
 
