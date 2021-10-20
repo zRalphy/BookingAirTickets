08b91438d89a90c22f42f7e8f62f0d413655a28c
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-10-20 09:35:48 +0200

    implemented test case for addUser method changed the @InjectedMock to constructor

--- src/test/java/com/pgs/booking/service/UserServiceTest.java
+++ src/test/java/com/pgs/booking/service/UserServiceTest.java
@@ -1,20 +1,30 @@
 package com.pgs.booking.service;
 
-import com.pgs.booking.model.User;
+import com.pgs.booking.mappers.dto.CreateUserDtoMapper;
+import com.pgs.booking.mappers.dto.RoleDtoMapper;
+import com.pgs.booking.mappers.dto.UserDtoMapper;
+import com.pgs.booking.mappers.entity.RoleEntityMapper;
+import com.pgs.booking.model.dto.CreateUserDto;
+import com.pgs.booking.model.dto.RoleDto;
+import com.pgs.booking.model.entity.Role;
+import com.pgs.booking.model.entity.User;
 import com.pgs.booking.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -22,15 +32,51 @@ import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
 
+    private static final Role ROLE = Role.builder()
+            .id(3L)
+            .name("USER")
+            .build();
+    private static final RoleDto ROLE_DTO = RoleDto.builder()
+            .id(3L)
+            .name("USER")
+            .build();
+    private static final List<Role> roleList = List.of(ROLE);
+    private static final List<RoleDto> roleDtoList = List.of(ROLE_DTO);
     private static final User USER = User.builder()
+            .id(2L)
             .username("user")
+            .roles(roleList)
+            .build();
+    private static final CreateUserDto CREATE_USER_DTO = CreateUserDto.builder()
+            .username("user")
+            .roles(roleDtoList)
             .build();
     @Mock
     private UserRepository userRepository;
-    @Mock
-    private TokenStore tokenStore;
-    @InjectMocks
     private UserService userService;
+
+    private RoleEntityMapper roleEntityMapper = new RoleEntityMapper();
+    private RoleDtoMapper roleDtoMapper = new RoleDtoMapper();
+    @Spy
+    private TokenStore tokenStore = new InMemoryTokenStore();
+    private CreateUserDtoMapper createUserDtoMapper = new CreateUserDtoMapper(roleEntityMapper);
+    private UserDtoMapper userDtoMapper = new UserDtoMapper(roleDtoMapper);
+
+    @BeforeEach
+    void setUp() {
+        userService = new UserService(userRepository, tokenStore, createUserDtoMapper, userDtoMapper);
+    }
+
+    @Test
+    void testAddUser() {
+        when(userRepository.save(any(User.class))).thenReturn(USER);
+        var user = userService.addUser(CREATE_USER_DTO);
+        verify(userRepository).save(any(User.class));
+        assertEquals(user.getId(), USER.getId());
+        assertEquals(user.getUsername(), USER.getUsername());
+        assertEquals(user.getRoles().get(0).getId(), USER.getRoles().get(0).getId());
+        assertEquals(user.getRoles().get(0).getName(), USER.getRoles().get(0).getName());
+    }
 
     @Test
     void testLoadUserByUsername() {
@@ -43,8 +89,8 @@ class UserServiceTest {
 
     @Test
     void testLoadByUsernameExceptionOccurrence() {
-        doReturn(USER)
-            .when(userRepository).findByUsername("user");
+        when(userRepository.findByUsername("user"))
+                .thenReturn(USER);
         userService.loadUserByUsername("user");
         Exception exception = assertThrows(UsernameNotFoundException.class, () ->
                 userService.loadUserByUsername("admin"));
@@ -64,17 +110,20 @@ class UserServiceTest {
         void setUp() {
             String tokenString = "QWERTY";
             var oAuth2Authentication = mock(OAuth2Authentication.class);
-
-            when(token.getPrincipal()).thenReturn(tokenString);
-            when(tokenStore.readAuthentication(tokenString)).thenReturn(oAuth2Authentication);
-            when(oAuth2Authentication.getUserAuthentication()).thenReturn(authentication);
+            when(token.getPrincipal())
+                    .thenReturn(tokenString);
+            when(tokenStore.readAuthentication(tokenString))
+                    .thenReturn(oAuth2Authentication);
+            when(oAuth2Authentication.getUserAuthentication())
+                    .thenReturn(authentication);
         }
 
         @Test
         void testLoadUserDetails() {
-            when(authentication.isAuthenticated()).thenReturn(true);
-            when(authentication.getPrincipal()).thenReturn(userDetails);
-
+            when(authentication.isAuthenticated())
+                    .thenReturn(true);
+            when(authentication.getPrincipal())
+                    .thenReturn(userDetails);
             var userDetailsService = userService.loadUserDetails(token);
             assertEquals(userDetails, userDetailsService);
 
