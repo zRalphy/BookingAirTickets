c9561aa974e8a44143e2a06f0982c7c1744e6cb9
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-10-20 10:46:30 +0200

    implemented endpoint for deactivating and activating user

--- src/main/java/com/pgs/booking/controller/UserController.java
+++ src/main/java/com/pgs/booking/controller/UserController.java
@@ -4,16 +4,13 @@ import com.pgs.booking.model.dto.CreateUserDto;
 import com.pgs.booking.model.dto.UserDto;
 import com.pgs.booking.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("api/user")
 @RequiredArgsConstructor
+@RequestMapping("api/user")
 public class UserController {
 
     private final UserService userService;
@@ -21,5 +18,9 @@ public class UserController {
     @PostMapping
     public UserDto addUser(@Valid @RequestBody CreateUserDto createUserDto) {
         return userService.addUser(createUserDto);
+    }
+    @PutMapping
+    public UserDto activateAndDeactivateUser(@PathVariable Long id , @PathVariable boolean isEnabled) {
+        return userService.activateAndDeactivateUser(id, isEnabled);
     }
 }
