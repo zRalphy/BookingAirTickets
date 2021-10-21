0b0b3d9645246899324c1f8ee0e8fdd86d465da1
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-10-21 10:59:07 +0200

    modified badly implemented lambda expression

--- src/main/java/com/pgs/booking/mappers/UserDtoMapper.java
+++ src/main/java/com/pgs/booking/mappers/UserDtoMapper.java
@@ -1,12 +1,11 @@
 package com.pgs.booking.mappers;
 
 
-import com.pgs.booking.model.entity.User;
 import com.pgs.booking.model.dto.UserDto;
+import com.pgs.booking.model.entity.Role;
+import com.pgs.booking.model.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-
-import java.util.Objects;
 
 @Component
 @RequiredArgsConstructor
@@ -16,7 +15,7 @@ public class UserDtoMapper {
     public UserDto mapToUserDto(User user) {
         var roleList = user.getRoles();
         var roleStringList = roleList.stream()
-                .map(Objects::toString)
+                .map(Role::getName)
                 .toList();
         return UserDto.builder()
                 .id(user.getId())
