5762272969aa330690468e88f605c03f9b5d8d8a
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-10-21 10:58:08 +0200

    modified the mapToUser method, removed redundant code

--- src/main/java/com/pgs/booking/mappers/CreateUserDtoMapper.java
+++ src/main/java/com/pgs/booking/mappers/CreateUserDtoMapper.java
@@ -1,27 +1,19 @@
 package com.pgs.booking.mappers;
 
-import com.pgs.booking.model.entity.User;
 import com.pgs.booking.model.dto.CreateUserDto;
-import lombok.RequiredArgsConstructor;
+import com.pgs.booking.model.entity.User;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class CreateUserDtoMapper {
 
-
-
     public User mapToUser(CreateUserDto createUserDto) {
-        var roleDtoList = createUserDto.getRoles();
-
         return User.builder()
                 .username(createUserDto.getUsername())
-                .password(createUserDto.getPassword())
                 .accountNonExpired(createUserDto.isAccountNonExpired())
                 .accountNonLocked(createUserDto.isAccountNonLocked())
                 .credentialsNonExpired(createUserDto.isCredentialsNonExpired())
                 .enabled(createUserDto.isEnabled())
-                .roles(roleDtoList)
                 .build();
     }
 }
