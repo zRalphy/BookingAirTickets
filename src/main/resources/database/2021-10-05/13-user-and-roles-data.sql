3e35b8fd6421400c788f58e448c98406de7d80ec
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-10-26 08:28:40 +0200

    deleted redundant line

--- src/main/resources/database/2021-10-05/13-user-and-roles-data.sql
+++ src/main/resources/database/2021-10-05/13-user-and-roles-data.sql
@@ -3,4 +3,3 @@
 insert into role (name) values ("STAFF");
 insert into user (username, password, accountNonExpired, accountNonLocked, credentialsNonExpired, enabled)
 values ("staff", "$2a$10$MQ3gurCRjaLMydFnWm32A.bRDNni9W6sJi/mwed6CfJBZjx3u1A3W", true, true, true, true);
-insert into user_roles(roles_id, user_id) values (3, 3);
