27b150c5018ae0162956adfd9571a9d7e06d68a0
Piotr Szerszeń <piotr.b.szerszen@icloud.com>
2021-10-27 15:26:27 +0200

    adding global request and response configuration to avoid copy-pasting

--- src/main/java/com/pgs/booking/configuration/swagger/SpringFoxConfig.java
+++ src/main/java/com/pgs/booking/configuration/swagger/SpringFoxConfig.java
@@ -1,11 +1,18 @@
 package com.pgs.booking.configuration.swagger;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.RequestParameterBuilder;
+import springfox.documentation.builders.ResponseBuilder;
+import springfox.documentation.service.ParameterType;
+import springfox.documentation.service.Response;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -13,24 +20,55 @@ import springfox.documentation.spring.web.plugins.Docket;
 public class SpringFoxConfig {
 
     private static final String DESCRIPTION = """
-            The Booking Air Tickets API is organized around REST. 
-            Our API is used for creating flight reservations by users.
-            It uses the Oauth 2 for authentication of the user.
-            When user successfully login to our API, 
-            he automatically send request for token and assign this token to him.
-            """;
+        The Booking Air Tickets API is organized around REST. 
+        Our API is used for creating flight reservations by users.
+        It uses the Oauth 2 for authentication of the user.
+        When user successfully login to our API, 
+        he automatically send request for token and assign this token to him.
+        """;
+
+    private static final List<Response> GENERIC_RESPONSES = List.of(
+        new ResponseBuilder()
+            .code("400")
+            .description("Invalid request")
+            .build(),
+        new ResponseBuilder()
+            .code("401")
+            .description("Request is not authenticated")
+            .build(),
+        new ResponseBuilder()
+            .code("403")
+            .description("User is not authorised for this action")
+            .build(),
+        new ResponseBuilder()
+            .code("500")
+            .description("Internal Server Error")
+            .build()
+        );
 
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(new ApiInfoBuilder()
-                        .title("Booking Air Tickects REST Service")
-                        .description(DESCRIPTION)
-                        .build())
-                .enable(true)
-                .ignoredParameterTypes(PreAuthenticatedAuthenticationToken.class)
-                .select()
-                .apis(RequestHandlerSelectors.withClassAnnotation(OpenAPIDefinition.class))
-                .build();
+            .apiInfo(new ApiInfoBuilder()
+                .title("Booking Air Tickects REST Service")
+                .description(DESCRIPTION)
+                .build())
+            .enable(true)
+            .ignoredParameterTypes(PreAuthenticatedAuthenticationToken.class)
+            .select()
+            .apis(RequestHandlerSelectors.withClassAnnotation(OpenAPIDefinition.class))
+            .build()
+            .useDefaultResponseMessages(true)
+            .globalRequestParameters(List.of(
+                new RequestParameterBuilder()
+                    .in(ParameterType.HEADER)
+                    .name(HttpHeaders.AUTHORIZATION)
+                    .description("Bearer token")
+                    .required(true)
+                    .build()))
+            .globalResponses(HttpMethod.GET, GENERIC_RESPONSES)
+            .globalResponses(HttpMethod.PUT, GENERIC_RESPONSES)
+            .globalResponses(HttpMethod.POST, GENERIC_RESPONSES)
+            .globalResponses(HttpMethod.DELETE, GENERIC_RESPONSES);
     }
 }
--- src/main/java/com/pgs/booking/controller/ReservationController.java
+++ src/main/java/com/pgs/booking/controller/ReservationController.java
@@ -8,14 +8,20 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import java.util.List;
+import javax.validation.Valid;
+import javax.websocket.server.PathParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-import javax.websocket.server.PathParam;
-import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @OpenAPIDefinition
 @RestController
@@ -26,12 +32,12 @@ public class ReservationController {
 
     private final ReservationService reservationService;
 
+    // MARK: - GETTING METHODS
+
     @Operation(summary = "Get all reservation for flight with given id")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Found all reservations"),
-            @ApiResponse(responseCode = "400", description = "Invalid flight id supplied"),
-            @ApiResponse(responseCode = "401", description = "Request is not authorize for this action"),
-            @ApiResponse(responseCode = "403", description = "User is not authorize for this action")
+        @ApiResponse(responseCode = "200", description = "Found all reservations"),
+        @ApiResponse(responseCode = "404", description = "Selected flight was not found")
     })
     @GetMapping("/flights/{id}")
     public List<ReservationDto> getReservationsByFlight(@PathVariable long id) {
@@ -40,10 +46,7 @@ public class ReservationController {
 
     @Operation(summary = "Get all reservation for current user")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Found all reservation for this user"),
-            @ApiResponse(responseCode = "400", description = "Invalid user id supplied"),
-            @ApiResponse(responseCode = "401", description = "Request is not authorize for this action"),
-            @ApiResponse(responseCode = "403", description = "User is not authorize for this action")
+        @ApiResponse(responseCode = "200", description = "Found all reservation for this user")
     })
     @PreAuthorize("hasAnyRole('ADMIN', 'STAFF', 'USER')")
     @GetMapping("/users")
@@ -56,21 +59,20 @@ public class ReservationController {
 
     @Operation(summary = "Get reservation for User")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Found all reservation for user"),
-            @ApiResponse(responseCode = "400", description = "Invalid user id supplied"),
-            @ApiResponse(responseCode = "401", description = "Request is not authorize for this action"),
-            @ApiResponse(responseCode = "403", description = "User is not authorize for this action")
+        @ApiResponse(responseCode = "200", description = "Found all reservation for user"),
+        @ApiResponse(responseCode = "404", description = "Selected user was not found")
     })
     @GetMapping("/users/{id}")
     public List<ReservationDto> getReservationsByUser(@PathVariable long id) {
         return reservationService.getReservationsByUser(id);
     }
 
+    // MARK: - ADDING METHODS
+
     @Operation(summary = "Add reservation")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Added reservation"),
-            @ApiResponse(responseCode = "400", description = "Invalid data passed"),
-            @ApiResponse(responseCode = "401", description = "Request is not authorize for this action")
+        @ApiResponse(responseCode = "200", description = "Added reservation"),
+        @ApiResponse(responseCode = "404", description = "Selected flight for reservation was not found")
     })
     @PreAuthorize("hasAnyRole('ADMIN', 'STAFF', 'USER')")
     @PostMapping
@@ -82,24 +84,24 @@ public class ReservationController {
         throw new IllegalStateException("The token does not contain authorized user data.");
     }
 
+    // MARK: - DELETE METHODS
+
     @Operation(summary = "Delete reservation by its id")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Deleted reservation"),
-            @ApiResponse(responseCode = "400", description = "Invalid reservation id supplied"),
-            @ApiResponse(responseCode = "401", description = "Request is not authorize for this action"),
-            @ApiResponse(responseCode = "403", description = "User is not authorize for this action")
+        @ApiResponse(responseCode = "200", description = "Deleted reservation"),
+        @ApiResponse(responseCode = "404", description = "Selected reservation was not found")
     })
     @DeleteMapping("/{id}")
     public void deleteReservation(@PathVariable long id) {
         reservationService.deleteReservation(id);
     }
 
+    // MARK: - PUT METHODS
+
     @Operation(summary = "Change reservation status with given id to REALIZED")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Changed reservation status to REALIZED"),
-            @ApiResponse(responseCode = "400", description = "Invalid reservation id supplied"),
-            @ApiResponse(responseCode = "401", description = "Request is not authorize for this action"),
-            @ApiResponse(responseCode = "403", description = "User is not authorize for this action")
+        @ApiResponse(responseCode = "200", description = "Changed reservation status to REALIZED"),
+        @ApiResponse(responseCode = "404", description = "Selected reservation was not found")
     })
     @PutMapping("/{id}/realized")
     public ReservationDto realizedReservation(@PathVariable Long id) {
@@ -108,10 +110,8 @@ public class ReservationController {
 
     @Operation(summary = "Change reservation status with given id to CANCELED")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Changed reservation status to CANCELED"),
-            @ApiResponse(responseCode = "400", description = "Invalid reservation id supplied"),
-            @ApiResponse(responseCode = "401", description = "Request is not authorize for this action"),
-            @ApiResponse(responseCode = "403", description = "User is not authorize for this action")
+        @ApiResponse(responseCode = "200", description = "Changed reservation status to CANCELED"),
+        @ApiResponse(responseCode = "404", description = "Selected reservation was not found")
     })
     @PutMapping("/{id}/canceled")
     public ReservationDto canceledReservation(@PathVariable Long id) {
