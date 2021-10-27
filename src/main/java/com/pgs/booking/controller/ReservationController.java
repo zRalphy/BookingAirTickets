a723b0dcbe1cb4dd3a425734329655bf9c5ff39f
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-10-27 11:54:56 +0200

    @Operation, @ApiResponses, @ApiResponse - to specify the summary of endpoints and to specified description of http response codes

--- src/main/java/com/pgs/booking/controller/ReservationController.java
+++ src/main/java/com/pgs/booking/controller/ReservationController.java
@@ -26,11 +26,23 @@ public class ReservationController {
 
     private final ReservationService reservationService;
 
+    @Operation(summary = "Get all reservation for flight with given id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found all reservations"),
+            @ApiResponse(responseCode = "400", description = "Invalid flight id supplied"),
+            @ApiResponse(responseCode = "401", description = "User unauthorized for this action")
+    })
     @GetMapping("/flights/{id}")
     public List<ReservationDto> getReservationsByFlight(@PathVariable long id) {
         return reservationService.getReservationsByFlight(id);
     }
 
+    @Operation(summary = "Get all reservation for current user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found all reservation for this user"),
+            @ApiResponse(responseCode = "400", description = "Invalid user id supplied"),
+            @ApiResponse(responseCode = "401", description = "User unauthorized for this action")
+    })
     @PreAuthorize("hasAnyRole('ADMIN', 'STAFF', 'USER')")
     @GetMapping("/users")
     public List<ReservationDto> getReservationsForCurrentUser(@PathParam("authenticationToken") PreAuthenticatedAuthenticationToken authenticationToken) {
@@ -40,11 +52,23 @@ public class ReservationController {
         throw new IllegalStateException("The token does not contain authorized user data.");
     }
 
+    @Operation(summary = "Get reservation for User")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found all reservation for user"),
+            @ApiResponse(responseCode = "400", description = "Invalid user id supplied"),
+            @ApiResponse(responseCode = "401", description = "User unauthorized for this action")
+    })
     @GetMapping("/users/{id}")
     public List<ReservationDto> getReservationsByUser(@PathVariable long id) {
         return reservationService.getReservationsByUser(id);
     }
 
+    @Operation(summary = "Add reservation")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Added reservation"),
+            @ApiResponse(responseCode = "400", description = "Invalid data passed"),
+            @ApiResponse(responseCode = "401", description = "User unauthorized for this action")
+    })
     @PreAuthorize("hasAnyRole('ADMIN', 'STAFF', 'USER')")
     @PostMapping
     public ReservationDto addReservation(@Valid @RequestBody CreateUpdateReservationDto createUpdatePassengerDto,
@@ -54,17 +78,34 @@ public class ReservationController {
         }
         throw new IllegalStateException("The token does not contain authorized user data.");
     }
-
+    @Operation(summary = "Delete reservation by its id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Deleted reservation"),
+            @ApiResponse(responseCode = "400", description = "Invalid reservation id supplied"),
+            @ApiResponse(responseCode = "401", description = "User unauthorized for this action")
+    })
     @DeleteMapping("/{id}")
     public void deleteReservation(@PathVariable long id) {
         reservationService.deleteReservation(id);
     }
 
+    @Operation(summary = "Change reservation status with given id to REALIZED")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Changed reservation status to REALIZED"),
+            @ApiResponse(responseCode = "400", description = "Invalid reservation id supplied"),
+            @ApiResponse(responseCode = "401", description = "User unauthorized for this action")
+    })
     @PutMapping("/{id}/realized")
     public ReservationDto realizedReservation(@PathVariable Long id) {
         return reservationService.realizedReservation(id);
     }
 
+    @Operation(summary = "Change reservation status with given id to CANCELED")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Changed reservation status to CANCELED"),
+            @ApiResponse(responseCode = "400", description = "Invalid reservation id supplied"),
+            @ApiResponse(responseCode = "401", description = "User unauthorized for this action")
+    })
     @PutMapping("/{id}/canceled")
     public ReservationDto canceledReservation(@PathVariable Long id) {
         return reservationService.canceledReservation(id);
