fb4aa08a97fa28c1a5a24606a9196e63aafe1c0e
Piotr Szerszeń <piotr.b.szerszen@icloud.com>
2021-10-27 16:25:04 +0200

    finished swagger documentation

--- src/main/java/com/pgs/booking/configuration/swagger/SpringFoxConfig.java
+++ src/main/java/com/pgs/booking/configuration/swagger/SpringFoxConfig.java
@@ -20,11 +20,23 @@ import springfox.documentation.spring.web.plugins.Docket;
 public class SpringFoxConfig {
 
     private static final String DESCRIPTION = """
-        The Booking Air Tickets API is organized around REST. 
-        Our API is used for creating flight reservations by users.
-        It uses the Oauth 2 for authentication of the user.
-        When user successfully login to our API, 
-        he automatically send request for token and assign this token to him.
+        The Booking Air Tickets API is organized around REST. Our API is used for creating flight reservations by users.
+        
+        ## Authentication
+        It uses the Oauth2 for authentication of the performed requests. In order to obtain a Bearer token for \
+        performing regular requests user need to first perform a request like this:
+        ```
+        curl -L -X POST 'http://localhost:8080/oauth/token' \\
+        -u {{ClientId}}:{{secret}} \\
+        -H 'Accept: application/json' \\
+        -H 'Content-Type: application/x-www-form-urlencoded' \\
+        --data-urlencode 'username={{user}}' \\
+        --data-urlencode 'password={{password}}' \\
+        --data-urlencode 'grant_type=password'
+        ```
+        `ClientId` and `secret` are credentials used for basic authentication. However, `user` and `password` are \
+        credentials of the user in database where roles are being assigned.
+        If request is successful, we should obtain token from the `access_token` field in response body.
         """;
 
     private static final List<Response> GENERIC_RESPONSES = List.of(
@@ -44,7 +56,7 @@ public class SpringFoxConfig {
             .code("500")
             .description("Internal Server Error")
             .build()
-        );
+    );
 
     @Bean
     public Docket api() {
