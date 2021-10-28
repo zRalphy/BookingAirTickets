79620accf6230a6839e8fb588b6074d50bdaad72
Sadymix <66955045+Sadymix@users.noreply.github.com>
2021-10-28 10:04:59 +0200

    Book 14 create documentation with swagger2 (#21)
    * added dependencies for swagger
    
    * implemented SpringFoxConfig for ussing swagger
    
    * ignore the oauth authentication for swagger endpoints
    
    * added version for springfox dependencies
    
    * configure swagger to show only the endpoints with @OpenAPIDefinition
    added the discription of API and title
    
    * @Operation, @ApiResponses, @ApiResponse - to specify the summary of endpoints and to specified description of http response codes
    
    * visual change
    
    * changed the description
    
    * @ApiResponse for 403 code and modified the description for 401 code
    
    * adding global request and response configuration to avoid copy-pasting
    
    * finished swagger documentation
    
    * simplifying configuration for security
    
    * configuring ResourceServer to omit swagger and actuator
    
    * deprecating manually adding passengers
    
    Co-authored-by: kkorzeniewski <kkorzeniewski@pgs-soft.com>
    Co-authored-by: Piotr Szerszeń <piotr.b.szerszen@icloud.com>

--- src/main/java/com/pgs/booking/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/pgs/booking/configuration/WebSecurityConfiguration.java
@@ -1,45 +1,63 @@
 package com.pgs.booking.configuration;
 
 
-import lombok.SneakyThrows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.core.GrantedAuthorityDefaults;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationProcessingFilter;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
-import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 
 @Configuration
 @EnableWebSecurity
-@EnableResourceServer
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    public WebSecurityConfiguration() {
+    static final String[] WHITE_LIST = {
+        "/v2/api-docs",
+        "/v3/api-docs",
+        "/swagger-resources/**",
+        "/configuration/**",
+        "/swagger-ui",
+        "/swagger-ui/**",
+        "/swagger-resources/**",
+        "/webjars/**",
+        "/actuator/**"
+    };
+
+    private final UserDetailsService userService;
+    private final PasswordEncoder passwordEncoder;
+    private final CustomAuthenticationProvider authenticationProvider;
+
+    public WebSecurityConfiguration(UserDetailsService userService,
+                                    PasswordEncoder passwordEncoder,
+                                    CustomAuthenticationProvider authenticationProvider) {
         super(true);
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+        this.authenticationProvider = authenticationProvider;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .antMatcher("/**")
-                .addFilterBefore(oAuth2AuthenticationProcessingFilter(), FilterSecurityInterceptor.class)
-                .authorizeRequests()
-                .anyRequest().authenticated();
+            .authorizeRequests()
+            .antMatchers(WHITE_LIST).permitAll()
+            .and()
+            .antMatcher("/api/**")
+            .authenticationProvider(authenticationProvider)
+            .authorizeRequests()
+            .anyRequest().authenticated();
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService)
+            .passwordEncoder(passwordEncoder);
     }
 
     @Override
@@ -47,24 +65,4 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
-
-    @Bean
-    public GrantedAuthorityDefaults grantedAuthorityDefaults() {
-        return new GrantedAuthorityDefaults("");
-    }
-
-    @Bean
-    @SneakyThrows
-    public OAuth2AuthenticationProcessingFilter oAuth2AuthenticationProcessingFilter() {
-        OAuth2AuthenticationProcessingFilter filter = new OAuth2AuthenticationProcessingFilter();
-        filter.setAuthenticationManager(authenticationManagerBean());
-        filter.setStateless(false);
-        return filter;
-    }
-
-    @Bean
-    public TokenStore tokenStore() {
-        return new InMemoryTokenStore();
-    }
-
 }
--- src/main/java/com/pgs/booking/controller/ReservationController.java
+++ src/main/java/com/pgs/booking/controller/ReservationController.java
@@ -4,15 +4,26 @@ import com.pgs.booking.model.dto.CreateUpdateReservationDto;
 import com.pgs.booking.model.dto.ReservationDto;
 import com.pgs.booking.model.entity.User;
 import com.pgs.booking.service.ReservationService;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import java.util.List;
+import javax.validation.Valid;
+import javax.websocket.server.PathParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-import javax.websocket.server.PathParam;
-import java.util.List;
-
+@OpenAPIDefinition
 @RestController
 @RequiredArgsConstructor
 @PreAuthorize("hasAnyRole('ADMIN', 'STAFF')")
@@ -21,11 +32,22 @@ public class ReservationController {
 
     private final ReservationService reservationService;
 
+    // MARK: - GETTING METHODS
+
+    @Operation(summary = "Get all reservation for flight with given id")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Found all reservations"),
+        @ApiResponse(responseCode = "404", description = "Selected flight was not found")
+    })
     @GetMapping("/flights/{id}")
     public List<ReservationDto> getReservationsByFlight(@PathVariable long id) {
         return reservationService.getReservationsByFlight(id);
     }
 
+    @Operation(summary = "Get all reservation for current user")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Found all reservation for this user")
+    })
     @PreAuthorize("hasAnyRole('ADMIN', 'STAFF', 'USER')")
     @GetMapping("/users")
     public List<ReservationDto> getReservationsForCurrentUser(@PathParam("authenticationToken") PreAuthenticatedAuthenticationToken authenticationToken) {
@@ -35,11 +57,23 @@ public class ReservationController {
         throw new IllegalStateException("The token does not contain authorized user data.");
     }
 
+    @Operation(summary = "Get reservation for User")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Found all reservation for user"),
+        @ApiResponse(responseCode = "404", description = "Selected user was not found")
+    })
     @GetMapping("/users/{id}")
     public List<ReservationDto> getReservationsByUser(@PathVariable long id) {
         return reservationService.getReservationsByUser(id);
     }
 
+    // MARK: - ADDING METHODS
+
+    @Operation(summary = "Add reservation")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Added reservation"),
+        @ApiResponse(responseCode = "404", description = "Selected flight for reservation was not found")
+    })
     @PreAuthorize("hasAnyRole('ADMIN', 'STAFF', 'USER')")
     @PostMapping
     public ReservationDto addReservation(@Valid @RequestBody CreateUpdateReservationDto createUpdatePassengerDto,
@@ -50,16 +84,35 @@ public class ReservationController {
         throw new IllegalStateException("The token does not contain authorized user data.");
     }
 
+    // MARK: - DELETE METHODS
+
+    @Operation(summary = "Delete reservation by its id")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Deleted reservation"),
+        @ApiResponse(responseCode = "404", description = "Selected reservation was not found")
+    })
     @DeleteMapping("/{id}")
     public void deleteReservation(@PathVariable long id) {
         reservationService.deleteReservation(id);
     }
 
+    // MARK: - PUT METHODS
+
+    @Operation(summary = "Change reservation status with given id to REALIZED")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Changed reservation status to REALIZED"),
+        @ApiResponse(responseCode = "404", description = "Selected reservation was not found")
+    })
     @PutMapping("/{id}/realized")
     public ReservationDto realizedReservation(@PathVariable Long id) {
         return reservationService.realizedReservation(id);
     }
 
+    @Operation(summary = "Change reservation status with given id to CANCELED")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Changed reservation status to CANCELED"),
+        @ApiResponse(responseCode = "404", description = "Selected reservation was not found")
+    })
     @PutMapping("/{id}/canceled")
     public ReservationDto canceledReservation(@PathVariable Long id) {
         return reservationService.canceledReservation(id);
