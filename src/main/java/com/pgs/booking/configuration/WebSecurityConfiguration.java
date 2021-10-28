b74c361a19de62b81dcc6b41ff4961a0ed66d9d8
Piotr Szerszeń <piotr.b.szerszen@icloud.com>
2021-10-28 09:43:42 +0200

    configuring ResourceServer to omit swagger and actuator

--- src/main/java/com/pgs/booking/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/pgs/booking/configuration/WebSecurityConfiguration.java
@@ -11,15 +11,13 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @Configuration
 @EnableWebSecurity
-@EnableResourceServer
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    private static final String[] WHITE_LIST = {
+    static final String[] WHITE_LIST = {
         "/v2/api-docs",
         "/v3/api-docs",
         "/swagger-resources/**",
@@ -67,5 +65,4 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
-
 }
